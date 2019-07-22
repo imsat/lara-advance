ae5ab9b67c66363709000cdb6407c4f848819307
imsat <worksatyajit@gmail.com>
2019-07-22 16:47:33 +0600

    move user model to App\Models directory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        factory('App\User')->create(['name' => 'Admin', 'email' => 'admin@admin.com', 'password' => bcrypt(123456)]);
+        factory('App\Models\User')->create(['name' => 'Admin', 'email' => 'admin@admin.com', 'password' => bcrypt(123456)]);
     }
 }
