082852362ecd977816b76dd23cc7d954bad423a8
imsat <satyajit.cse@yahoo.com>
2021-08-30 11:00:50 +0600

    Update composer lock file

--- public/js/app.js
+++ public/js/app.js
@@ -60726,8 +60726,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\laragon\www\lara-advance\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\laragon\www\lara-advance\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/lara-advance/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/lara-advance/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
